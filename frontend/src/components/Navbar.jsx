e5f9867d3dc65a32e4f00bbe11e989e189e75f19
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-09 20:47:13 +0545

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import react from '../assets/react.svg'
+import Logo from '../assets/logo.png'
 import { Link } from "react-router-dom"
 
 export default function Navbar() {
@@ -11,12 +11,12 @@ export default function Navbar() {
   return (
 		<nav className="w-full flex py-6 justify-between items-center px-8">
 			<div className=" flex flex-start ml-28">
-				<img src={react} alt=""/>
+				<img src={Logo} height={60} width={60} alt="logo"/>
 			</div>
 			<div className=" justify-center items-center mr-28">
 				<ul className=" flex gap-12 list-none">
 					{navbarItems.map((nav, index) => (
-						<li key={index} className='font-roboto font-semibold cursor-pointer'>
+						<li key={index} className='font-roboto font-semibold cursor-pointer hover:text-purple-400'>
 						<Link to={nav.path}>{nav.link}</Link>
 						</li>
 					))}
