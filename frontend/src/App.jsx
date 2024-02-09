41618128d080663d1d9bf0be86e8237afff47cae
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-09 19:06:03 +0545

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
 import './App.css'
+import AppRoutes from './routes/routes'
 
 function App() {
   return (
     <>
      <Navbar/>
-     <Landing/>
+     <AppRoutes />
     </>
   )
 }
--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,18 +1,13 @@
 export default function Landing() {
   return (
-      <div className="flex items-center justify-center w-100 mt-10">
-        <div className="mx-auto ">
-          <div className="flex-1 shadow-lg px-8 p-8 flex bg-blue-200 ">
-            <div className="flex-1">
-              <h1 className="text-3xl font-bold mb-4">Your Web App Title</h1>
-              <p className="text-gray-700">
-                Your Web App Description goes here. Provide a brief overview of your app and its features.
-              </p>
-            </div>
-            <div className="flex-1">
-            </div>
-          </div>
+    <section className="flex md:flex-row flex-col px-8 mt-10">
+      <div className="flex-1 flex-start flex-col mx-[100px]">
+        <div className="flex flex-row items-center py-[6px] px-4">
+          <span>Vehicle Detection and</span>
+          <span>Counting</span>
         </div>
       </div>
+
+    </section>
   )
 }
