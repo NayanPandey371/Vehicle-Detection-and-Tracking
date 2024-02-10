ae06db8cbb99a1c0b2e8a88793269dafd6fefe85
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-10 17:38:36 +0545

    hero section complete

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,30 +1,29 @@
-import logo from '../assets/logo.png'
+import hero from '../assets/hero.jpg'
 
 export default function Landing() {
   return (
-    <div className="max-w-screen-2xl mx-20 mt-16">
+    <div className="max-w-screen-2xl mx-20 mt-10">
       <div className='gradientBg rounded-xl rounded-br-[80px]'>
         <div className="flex flex-row justify-between items-center gap-1">
 
-          <div className='p-4 ml-4 '>
+          <div className='px-4 ml-4 '>
             <h2 className='text-4xl text-white mb-4'>Vehicle Detection</h2>
             <h2 className='text-4xl text-white mb-4'>and</h2>
             <h2 className='text-4xl text-white mb-4'>Counting</h2>
-            <div className='w-2/3 mt-[6px] mb-[30px]'>
+            {/* <div className='w-2/3 mt-[6px] mb-[30px]'> */}
               <p className='text-white text-[12px] w-2/3'>
                 Ever wished you could monitor your property or fleet of vehicles with eagle-eyed accuracy? Look no further! 
                 This web application leverages the power of YOLO (You Only Look Once) to detect and track vehicles,
                 providing you with invaluable insights and security.
               </p>
-            </div>
-            
-            <div>
-              <button className="py-2 px-4 bg-primary text-white rounded cursor-pointer ">Get Started</button>
+            <div className='mt-4'>
+
+              <button className="py-2 px-4 bg-primary text-white rounded cursor-pointer hover:shadow-boxshadowcolor" >Get Started</button>
             </div>
           </div>
 
-          <div>
-            <img src={logo} className='h-40 w-40'/>
+          <div> 
+            <img src={hero} className='rounded-br-[80px]'/>
           </div>
         </div>
       </div>
