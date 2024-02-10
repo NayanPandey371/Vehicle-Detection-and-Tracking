ba2c62b620327b5dbc15dff276301c042bbe1cf2
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-10 18:13:08 +0545

    footer section complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar'
 import Landing from './components/Landing'
 import './App.css'
 import AppRoutes from './routes/routes'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
      <Navbar/>
      <AppRoutes />
+     <Footer/>
     </>
   )
 }
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home'
 import Landing from '../components/Landing';
+
 const AppRoutes = () => {
   return (
       <Routes>
