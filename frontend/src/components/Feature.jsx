9948b2dc28368bbd2195d423bf92c6a5c715dabf
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-11 22:07:17 +0545

    added styling

--- frontend/src/components/Feature.jsx
+++ frontend/src/components/Feature.jsx
@@ -9,24 +9,23 @@ export default function Feature() {
             <h2 className="text-center text-bold text-4xl text-primary">Features</h2>
         </div>
         {/* Detection  */}
-        <div className="flex flex-row justify-between items-center gap-1 ml-4">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-1 ml-4">
             <div className='w-1/2 px-4'>
-                <img src={Detection} alt="Hero image" />
+                <img src={Detection} alt="Detection image" />
             </div>
 
             <div className='w-2/5'>
                 <h2 className='text-semibold text-3xl text-primary mb-4'>Detection and Counting</h2>
-                <p>Detection and counting of vehicle based on video using YOLO. 
+                <p className='text-roboto text-[18px]'>Detection and counting of vehicle based on video using YOLO. 
                 Our advanced YOLO model effortlessly detects individual vehicles within the video, ensuring high accuracy regardless of weather conditions or lighting variations.</p>
             </div>
         </div>
         {/* Classification  */}
-        <div className="flex flex-row justify-between items-center gap-1 ml-4">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-1 ml-4">
             <div className='w-2/5'>
                 <h2 className='text-semibold text-3xl text-primary mb-4'>Classification of Vehicles</h2>
-                <p>Classification of vehicles based on nine categories.
-                    Beyond simple detection, our app goes further by classifying each vehicle into nine distinct categories: car, truck, motorcycle, bus, bicycle, and more.
-                    that provides deeper understanding of traffic composition.</p>
+                <p className='text-roboto text-[18px]'>Classification of vehicles based on six categories.
+                    Beyond simple detection, our app goes further by classifying each vehicle into six distinct categories: Car, Truck, 2-Wheeler, Bus, MiniBus and Tempo.</p>
             </div>
 
             <div className='w-1/2 px-4'>
@@ -34,14 +33,14 @@ export default function Feature() {
             </div>
         </div>
 
-        <div className="flex flex-row justify-between items-center gap-1 ml-4">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-1 ml-4">
             <div className='w-1/2 px-4'>
-                <img src={Piechart} alt="Hero image" />
+                <img src={Piechart} alt="Pie Chart image" />
             </div>
 
             <div className='w-2/5'>
                 <h2 className='text-semibold text-3xl text-primary mb-4'>Informative Analytics </h2>
-                <p>Dive into a comprehensive dashboard packed with valuable insights derived from your video data.
+                <p className='text-roboto text-[18px]'>Dive into a comprehensive dashboard packed with valuable insights derived from your video data.
                     Track total vehicle counts, analyze trends over time, and gain insights into the distribution of different vehicle types.
                     The data can be used for further analysis and reporting, empowering informed decision-making.</p>
             </div>
