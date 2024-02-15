ec0e99f1c41e81f08ef38f7a14630858253ea48e
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-15 20:03:29 +0545

    adding detect page

--- frontend/src/pages/Detect.jsx
+++ frontend/src/pages/Detect.jsx
@@ -1,5 +1,23 @@
 export default function Detect() {
   return (
-    <div>Detect</div>
+    <div className="w-full flex justify-center items-center">
+      <div className="w-96 flex flex-col justify-center items-center ">
+      <div>
+        <h2 className="text-center text-semibold text-2xl my-4">Upload Your Video</h2>
+        <p>
+          We support MP4 video format.
+        </p>
+      </div>
+      <div>
+
+      </div>
+      <div className="w-full">
+        <button className="w-full py-2 px-4 mt-4 bg-primary text-white rounded cursor-pointer hover:shadow-boxshadowcolor">Upload</button>
+      </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
   )
 }
