a7e783dfb69773e18e89eaf3e82fc497e8c7aabf
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-23 14:36:28 +0545

    video uploading using axois

--- frontend/src/pages/Detect.jsx
+++ frontend/src/pages/Detect.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Dropzone from '../components/Dropzone'
+import axios from 'axios'
 
 export default function Detect() {
 
@@ -9,6 +10,32 @@ export default function Detect() {
   const onFilesSelected = (newFiles) => {
     setFiles(newFiles)
   }
+
+  const handleVideoSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!files) {
+      console.error('Please select a video file');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('video', files);
+
+    try {
+      const response = await axios.post('127.0.0.1:8000/upload-video', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log('Video uploaded successfully:', response.data);
+      // Handle successful upload (e.g., clear form, display success message)
+    } catch (error) {
+      console.error('Error uploading video:', error);
+      // Handle upload errors (e.g., display error message)
+    }
+  };
   
   useEffect(() => {
     // Handle dropped files when they change
@@ -23,8 +50,7 @@ export default function Detect() {
       videos.forEach((url) => URL.revokeObjectURL(url));
     };
   }, [files]);
-  // console.log(videoURL)
-  // console.log(files[0].path)
+ 
   return (
     <div className="w-full flex justify-center items-center">
       <div className="w-96 flex flex-col justify-center items-center ">
@@ -38,11 +64,11 @@ export default function Detect() {
         <Dropzone onFilesSelected={onFilesSelected}/>
       </div>
       <div className="w-full">
-        <button className="w-full py-2 px-4 mt-4 bg-primary text-white rounded cursor-pointer hover:shadow-boxshadowcolor">Upload</button>
+        <button className="w-full py-2 px-4 mt-4 bg-primary text-white rounded cursor-pointer hover:shadow-boxshadowcolor" onClick={handleVideoSubmit}>Upload</button>
       </div>
-      {videos.map((videoUrl, index) => (
+      {/* {videos.map((videoUrl, index) => (
       <video key={index} src={videoUrl} controls />
-      ))}
+      ))} */}
       </div>
     </div>
   )
