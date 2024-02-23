06fb23c098c0171c8303c9c17e276fd71d6966dd
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-23 15:49:49 +0545

    video uploaded to backend

--- app/api.py
+++ app/api.py
@@ -1,10 +1,38 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, UploadFile, File
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+# Create a list of allowed origins
+origins = ["http://localhost:5173"]
 
 app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+
+# Add the CORSMiddleware to the app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # Allow cookies and other credentials
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all request headers
+)
+
+@app.get('/')
+def sayhello():
+    return{"Hello"}
+
+@app.post("/upload-video")
+async def upload_video(video: UploadFile = File(...)):
+    try:
+        content = await video.read()
+
+        # # Save the video file (replace with your desired logic)
+        # with open(f"uploads/{video.filename}", "wb") as f:
+        #     f.write(content)
+        print(video.filename)
+        return {"message": "Video uploaded successfully!"}
+
+    except Exception as e:
+        return {"message": f"An error occurred: {str(e)}"}
 
 # Run the FastAPI server
 if __name__ == "__main__":
