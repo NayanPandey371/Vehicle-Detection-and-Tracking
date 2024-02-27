364c9824dbdf56cb69e100ba2bc252f591261673
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-27 20:47:47 +0545

    embedded custom model

--- app/api.py
+++ app/api.py
@@ -1,12 +1,21 @@
+import sys
+sys.path.insert(0, './model')
+
 from fastapi import FastAPI, UploadFile, File
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-
+import torch
+import os
 # Create a list of allowed origins
 origins = ["http://localhost:5173"]
 
 app = FastAPI()
+print(torch.__version__)
 
+model_path = './model/best.pt'
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
+print(model)
+# print(model)
 # Add the CORSMiddleware to the app
 app.add_middleware(
     CORSMiddleware,
@@ -19,6 +28,8 @@ app.add_middleware(
 @app.get('/')
 def sayhello():
     return{"Hello"}
+
+
 
 @app.post("/upload-video")
 async def upload_video(video: UploadFile = File(...)):
@@ -33,6 +44,7 @@ async def upload_video(video: UploadFile = File(...)):
 
     except Exception as e:
         return {"message": f"An error occurred: {str(e)}"}
+
 
 # Run the FastAPI server
 if __name__ == "__main__":
