56327aeeaa86263665ed503e03b92fb89fcad497
PRASUN-SITAULA <psitaula209@gmail.com>
2024-03-07 18:00:41 +0545

    video playback complete

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,33 +1,41 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
-import Loader from "../components/Loader"
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
+import ReactPlayer from 'react-player'
+import { Player } from 'video-react';
 
 export default function Result() {
 
-    const [isVideo, setIsVideo] = useState(false)
-    const [videoSrc, setVideoSrc] = useState()
 
-    useEffect(() => {
-        const getVideo = async () => {
-            try{
-                const response = await axios.get('http://127.0.0.1:8000/get-result')
-                console.log("Message", response.data)
-            }catch(err){
-                console.log(err)
-            }
-            finally{
-                const video_path = '../../../output/out.mp4'
-                setVideoSrc(video_path)
-                setIsVideo(true)
-            }
-        }
-        getVideo()
-    }, [])
+    const [isVideo, setIsVideo] = useState(true)
+
+    // useEffect(() => {
+    //     const getVideo = async () => {
+    //         try{
+    //             const response = await axios.get('http://127.0.0.1:8000/get-result')
+    //             setIsVideo(true)
+    //             console.log(response.data)
+    //         }catch(err){
+    //             console.log(err)
+    //         }
+    //     }
+    //     getVideo()
+    //     // Clean up temporary URL on component unmount
+    //     //  return () => URL.revokeObjectURL(videoUrl);
+    // }, [])
 
   return (
-    <div>
-    { !isVideo && <Loader/>}
-    hello
+    <div className="max-w-screen-2xl mx-20 mt-10">
+        <div className="flex flex-col justify-center align-center">
+            { !isVideo ? (
+                <Skeleton containerClassName="flex-1" baseColor='#7734e7' highlightColor="#EAE8E7" count={7} />
+            ):
+            (<ReactPlayer
+                url="/videos/out.mp4"
+                controls
+            />)}
+        </div>
     </div>
   )
 }
