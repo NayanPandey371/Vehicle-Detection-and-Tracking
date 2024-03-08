d4843c427f81e9d9677c8dd19d43ffa20840c7b6
PRASUN-SITAULA <psitaula209@gmail.com>
2024-03-08 17:20:22 +0545

    updated pie chart

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -7,24 +7,25 @@ import PieChart from "../components/PieChart";
 
 export default function Result() {
 
-    const [isVideo, setIsVideo] = useState(true)
+    const [isVideo, setIsVideo] = useState(false)
+    const [videoUrl, setVideoUrl] = useState('')
     const [northCount, setNorthCount] = useState({})
     const [southCount, setSouthCount] = useState({})
 
-    // useEffect(() => {
-    //     const getVideo = async () => {
-    //         try{
-    //             const response = await axios.get('http://127.0.0.1:8000/get-result')
-    //             setIsVideo(true)
-    //             setNorthCount(response.data["north_count"])
-    //             setSouthCount(response.data["south_count"])
-    //             console.log(response.data["north_count"])
-    //         }catch(err){
-    //             console.log(err)
-    //         }
-    //     }
-    //     getVideo()
-    // }, [])
+    useEffect(() => {
+        const getVideo = async () => {
+            try{
+                const response = await axios.get('http://127.0.0.1:8000/get-result')
+                setIsVideo(true)
+                setVideoUrl('/videos/out.mp4')
+                setNorthCount(response.data["north_count"])
+                setSouthCount(response.data["south_count"])
+            }catch(err){
+                console.log(err)
+            }
+        }
+        getVideo()
+    }, [])
 
   return (
     <div className="max-w-screen-2xl mx-20 mt-10">
@@ -33,26 +34,25 @@ export default function Result() {
                     <Skeleton containerClassName="flex-1" baseColor='#E8CBF4' highlightColor="#EAE8E7" count={6} height={20}/>
                     <Skeleton containerClassName="flex-1" baseColor='#E8CBF4' highlightColor="#EAE8E7" width="50%" height={20}/>
                 </div>    
-            ):
+            ):(
             <div className="flex flex-col justify-center align-center">
                 <div className="flex justify-center align-center">
-                    <ReactPlayer url='/videos/out.mp4'  controls />
+                    <ReactPlayer url={videoUrl}  controls />
                 </div>
                 <div className="flex justify-center align-center">
                     <div className="flex flex-row justify-center align-center gap-16 mt-10 ">
                         <div className="chartGradientBg rounded-3xl p-10">
-                            <PieChart />
+                            <PieChart data={northCount}/>
                             <h3 className="text-2xl font-bold text-center mt-2 text-black">North</h3>
                         </div>
                         <div className="chartGradientBg rounded-3xl p-10">
-                            <PieChart />
+                            <PieChart data={southCount}/>
                             <h3 className="text-2xl font-bold text-center mt-2 text-black">South</h3>                                
                         </div>
-
                     </div>
                 </div>
             </div>
-            }
+            )}
         </div>
   )
 }
